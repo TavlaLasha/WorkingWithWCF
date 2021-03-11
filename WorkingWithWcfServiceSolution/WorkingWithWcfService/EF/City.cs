9eb6e5e7566efb08b37774a2450121dc3186d5c9
aivazashvilitekla <tekla.aivazashvili@gau.edu.ge>
2021-03-11 19:56:41 +0400

    Import DBContext

---
+++ WorkingWithWcfServiceSolution/WorkingWithWcfService/EF/City.cs
@@ -0,0 +1,31 @@
+namespace WorkingWithWcfService.EF
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Catalogs.Cities")]
+    public partial class City
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public City()
+        {
+            Employees = new HashSet<Employee>();
+        }
+
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public int CountryID { get; set; }
+
+        public virtual Country Country { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Employee> Employees { get; set; }
+    }
+}
