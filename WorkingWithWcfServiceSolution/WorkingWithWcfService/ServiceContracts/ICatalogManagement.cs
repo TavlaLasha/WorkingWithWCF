4c5677baea76a04864a77b3c0a36795541dc3269
TavlaLasha <lashatavlalashvili@yahoo.com>
2021-03-11 20:54:44 +0400

    Added Catalog Management / Second time

---
+++ WorkingWithWcfServiceSolution/WorkingWithWcfService/ServiceContracts/ICatalogManagement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WorkingWithWcfService.ServiceContracts
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ICatalogManagement" in both code and config file together.
+    [ServiceContract]
+    public interface ICatalogManagement
+    {
+        [OperationContract]
+        void DoWork();
+    }
+}
