2b0fbdd142b7b97817424fcac74ea9950ea4a137
aivazashvilitekla <aivazashvilitekla7@gmail.com>
2021-03-15 20:17:30 +0400

    added supplier cruds

---
+++ WCFClientSolution/WCFClient/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace WCFClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_supplier_by_id = new System.Windows.Forms.Button();
+            this.btn_get_all = new System.Windows.Forms.Button();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(745, 495);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btn_supplier_by_id
+            // 
+            this.btn_supplier_by_id.Location = new System.Drawing.Point(777, 45);
+            this.btn_supplier_by_id.Name = "btn_supplier_by_id";
+            this.btn_supplier_by_id.Size = new System.Drawing.Size(188, 61);
+            this.btn_supplier_by_id.TabIndex = 1;
+            this.btn_supplier_by_id.Text = "Get Supplier by ID";
+            this.btn_supplier_by_id.UseVisualStyleBackColor = true;
+            this.btn_supplier_by_id.Click += new System.EventHandler(this.btn_supplier_by_id_Click);
+            // 
+            // btn_get_all
+            // 
+            this.btn_get_all.Location = new System.Drawing.Point(777, 189);
+            this.btn_get_all.Name = "btn_get_all";
+            this.btn_get_all.Size = new System.Drawing.Size(188, 29);
+            this.btn_get_all.TabIndex = 2;
+            this.btn_get_all.Text = "Get All Suppliers";
+            this.btn_get_all.UseVisualStyleBackColor = true;
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(777, 310);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(188, 29);
+            this.btn_add.TabIndex = 3;
+            this.btn_add.Text = "Add Supplier";
+            this.btn_add.UseVisualStyleBackColor = true;
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Location = new System.Drawing.Point(777, 478);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(188, 29);
+            this.btn_delete.TabIndex = 4;
+            this.btn_delete.Text = "Delete";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(777, 12);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(188, 27);
+            this.numericUpDown1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(977, 519);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.btn_delete);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.btn_get_all);
+            this.Controls.Add(this.btn_supplier_by_id);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_supplier_by_id;
+        private System.Windows.Forms.Button btn_get_all;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Button btn_delete;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
